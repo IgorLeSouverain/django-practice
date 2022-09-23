528c6a6b5f57ec2d68abdaa210efb84642d2d7f5
Igor <braguntsov10@gmail.com>
2022-09-23 20:39:54 +0300

    update

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Room(models.Model):
   #host = 
   #topic = 
@@ -14,3 +15,14 @@ class Room(models.Model):
 
   def __str__(self):
     return self.name
+
+
+class Message(models.Model):
+  #user = 
+  room = models.ForeignKey(Room, on_delete=models.CASCADE)
+  body = models.TextField()
+  updated = models.DateTimeField(auto_now=True)
+  created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.body[0:50]
