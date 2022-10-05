9f780b0dd0d711418045ba708ef5dbf367a5b03a
Igor <braguntsov10@gmail.com>
2022-10-05 17:59:40 +0300

    update

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,14 @@
 from dataclasses import field
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, User
+
+
+class MyUserCreationForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ['name', 'username', 'email', 'password1', 'password2']
+
 
 class RoomForm(ModelForm):
   class Meta:
@@ -8,7 +16,8 @@ class RoomForm(ModelForm):
     fields = '__all__'
     exclude = ['host', 'participants']
 
+
 class UserForm(ModelForm):
   class Meta:
     model = User
-    fields = ['username', 'email']
+    fields = ['name', 'bio', 'avatar' , 'username', 'email']
